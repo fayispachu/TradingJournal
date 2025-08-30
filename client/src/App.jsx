7f82a548fbc2b1980e157cdc389c1da19a12e18b
Fayis <fayizpachu217@gmail.com>
2025-08-30 18:21:36 +0530

    conext filename changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             <BottomNavbar />
           </UserContextProvider>
         </OtherContextProvider>
-      </BrowserRouter>{" "}
+      </BrowserRouter>
     </>
   );
 }
