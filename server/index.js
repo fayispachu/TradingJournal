f481d383e3627f5f02a15cdcbaf53b7e0a1acd5b
Fayis <fayizpachu217@gmail.com>
2025-08-30 18:07:57 +0530

    build

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./db/dataBase.js";
+
 import UserRouter from "./routes/User.route.js";
 import TradeRouter from "./routes/Trade.route.js";
 import AccountOverviewRouter from "./routes/AccountOverview.route.js";
@@ -10,18 +13,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://your-app.onrender.com"],
     credentials: true,
   })
 );
+
 app.use(express.json());
+
 connectDB();
 
 app.use("/api/user", UserRouter);
 app.use("/api/trade", TradeRouter);
 app.use("/api/account-overview", AccountOverviewRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
